8499357b7d84adc03a170a0c3b3a0e897b14d129
MatheusMA678 <matheusma2510@hotmail.com>
2023-04-24 20:19:16 -0400

    Arrumando erros.

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Loading() {
-  return (
-    <div className="fixed top-4 left-4 text-white font-bold text-2xl">
-      Carregando...
-    </div>
-  );
-}
